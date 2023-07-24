d4b59fdef8359742c545e40a9447ec9da0e83a18
Bilge Yücel <bilgeyucel96@gmail.com>
2023-07-24 17:37:44 +0100

    Run tutorials on latest Haystack version + on main (#224)
    * Add script to generate matrix of testable notebooks
    
    * Add generate_matrix.py to the nightly workflow
    
    * add gh token
    
    * Update API Key handling for tut 25
    
    * Marks some tutorials as "needs gpu" not to run them on nightly tests
    
    * Run tutorials on main branch in nightly tests
    
    * Fix handle for tutorial that has no colab
    
    ---------
    
    Co-authored-by: Silvano Cerza <silvanocerza@gmail.com>

---
+++ scripts/generate_matrix.py
@@ -0,0 +1,50 @@
+import tomllib
+import argparse
+import json
+
+
+def read_index(path):
+    with open(path, "rb") as f:
+        return tomllib.load(f)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(usage="""python generate_matrix.py --haystack-version v1.18.1""")
+    parser.add_argument("--index", dest="index", default="index.toml")
+    parser.add_argument("--notebooks", dest="notebooks", nargs="+", default=[])
+    parser.add_argument("--haystack-version", dest="version", required=True)
+    parser.add_argument("--include-main", dest="main", action="store_true")
+
+    args = parser.parse_args()
+    index = read_index(args.index)
+
+    matrix = []
+    for tutorial in index["tutorial"]:
+        notebook = tutorial["notebook"]
+
+        if args.notebooks and notebook not in args.notebooks:
+            # If the user specified a list of notebooks to run, only run those
+            # otherwise run all of them
+            continue
+
+        if tutorial.get("needs_gpu", False):
+            # We're not running GPU tutorials on GitHub Actions
+            # since we don't have a GPUs there
+            continue
+
+        if not tutorial.get("colab", True):
+            # This tutorial doesn't have any runnable Python code
+            # so there's nothing to test
+            continue
+
+        version = tutorial.get("haystack_version", args.version)
+        if version[0] != "v":
+            version = f"v{version}"
+
+        matrix.append({"notebook": notebook[:-6], "haystack_version": version})
+
+        if args.main and "haystack_version" not in tutorial:
+            # If a tutorial doesn't specify a version, we also test it on main
+            matrix.append({"notebook": notebook[:-6], "haystack_version": "main"})
+
+    print(json.dumps(matrix))
